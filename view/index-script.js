97695a84dc028133c1330b144123b5fa9b515f27
BertAtibagos <atibagos.robert2300@gmail.com>
2026-03-02 14:38:35 +0800

    Added subject filter component

--- view/index-script.js
+++ view/index-script.js
@@ -1,9 +1,12 @@
-import { tableComponent } from './index-component.js';
+import { tableComponent } from '../components/record-cards.js';
+import { subjectFilterComponent } from '../components/subject-filter.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
     const attendance = document.getElementById('attndnc_logs_card');
+    const subjectFilter = document.getElementById('subjectFilter');
     
-    if (attendance) {
+    if (attendance && subjectFilter) {
         attendance.innerHTML = await tableComponent();
+        subjectFilter.innerHTML = await subjectFilterComponent();
     }
 });

260b4eccbe8712d14935195e6937267527d35b56
BertAtibagos <atibagos.robert2300@gmail.com>
2026-03-05 16:25:36 +0800

    Added eventhandler for changing subject filter

--- view/component-loader.js
+++ view/component-loader.js
@@ -1,12 +1,19 @@
 import { tableComponent } from '../components/record-cards.js';
 import { subjectFilterComponent } from '../components/subject-filter.js';
 
+const attendance = document.getElementById('attndnc_logs_card');
+
 document.addEventListener('DOMContentLoaded', async () => {
-    const attendance = document.getElementById('attndnc_logs_card');
     const subjectFilter = document.getElementById('subjectFilter');
     
     if (attendance && subjectFilter) {
         attendance.innerHTML = await tableComponent();
         subjectFilter.innerHTML = await subjectFilterComponent();
     }
+});
+
+document.getElementById('subjectSelect').addEventListener('change', async (e)=>{
+    const selectedSubject = e.target.value;
+
+    attendance.innerHTML = await tableComponent(selectedSubject);
 });
